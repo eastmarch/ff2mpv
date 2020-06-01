988d202e05745aafee9a1599ec04fd18ad2af4ed
Daktyl <daktyl@orinjido.info>
2020-06-01 00:12:42 -0400

    Code cleanup, as well as added functionality

--- ff2mpv.js
+++ ff2mpv.js
@@ -1,28 +1,19 @@
 function ff2mpv(url) {
-    var link = url.replace(new RegExp("^.*//"),"ytdl://")
-    browser.tabs.executeScript({
-        code: `window.location = "` + link + `";`
-    });
+  const link = url.replace(/^(https?|ftp):\/\//, 'ytdl://');
+  browser.tabs.executeScript({code: `window.location = "${link}";`});
 }
 
 browser.contextMenus.create({
-    id: "ff2mpv",
-    title: "Play in MPV",
-    contexts: ["link", "frame"]
+    id: 'ff2mpv',
+    title: 'Play in MPV',
+    contexts: ['link', 'frame', 'tab', 'video', 'selection']
 });
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
-    switch (info.menuItemId) {
-        case "ff2mpv":
-            /* These should be mutually exclusive, but,
-               if they aren't, this is a reasonable priority.
-            */
-            url = info.linkUrl || info.frameUrl;
-            if (url) ff2mpv(url);
-            break;
+browser.contextMenus.onClicked.addListener(info => {
+    if (info.menuItemId === 'ff2mpv') {
+        const url = info.linkUrl || info.srcUrl || info.frameUrl || info.selectionText || info.pageUrl;
+        if (url) ff2mpv(url);
     }
 });
 
-browser.browserAction.onClicked.addListener((tab) => {
-    ff2mpv(tab.url);
-});
+browser.browserAction.onClicked.addListener(tab => ff2mpv(tab.url));
