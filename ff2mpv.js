4212d9bd121375a9189b142c6e7b82e5722b6a07
eastmarch <29557084+eastmarch@users.noreply.github.com>
2025-04-21 22:09:09 -0300

    feat: manifest v3

--- ff2mpv.js
+++ ff2mpv.js
@@ -1,21 +1,36 @@
-function ff2mpv(url) {
-  var link = url.replace(new RegExp('^(http|https)://', 'i'), 'ytdl://');
-  browser.tabs.executeScript({ code: `window.location = "${link}";` });
-}
+'use strict';
 
 browser.contextMenus.create({
   id: 'ff2mpv',
-  title: 'Play in MPV',
-  contexts: ['link', 'frame', 'tab'],
+  title: 'Play link in MPV',
+  contexts: ['link'],
 });
 
-browser.contextMenus.onClicked.addListener((info) => {
-  if (info.menuItemId === 'ff2mpv') {
-    const url = info.linkUrl || info.frameUrl || info.pageUrl;
-    if (url) ff2mpv(url);
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId == 'ff2mpv') {
+    playInMPV(tab.id, info.linkUrl, false);
   }
 });
 
-browser.browserAction.onClicked.addListener((tab) => {
-  ff2mpv(tab.url);
+browser.action.onClicked.addListener((tab) => {
+  playInMPV(tab.id, tab.url, true);
 });
+
+function playInMPV(tabID, targetURL, pausePlayback) {
+  let link = targetURL.replace(new RegExp('^(http|https)://', 'i'), 'ytdl://');
+  browser.scripting.executeScript({
+    target: { tabId: tabID },
+    func: redirectMPV,
+    args: [link, pausePlayback],
+  });
+}
+
+function redirectMPV(redirectURL, pausePlayback) {
+  window.location.href = redirectURL;
+  if (pausePlayback) {
+    const pageVideos = document.querySelectorAll('video');
+    for (const video of pageVideos) {
+      video.pause();
+    }
+  }
+}
